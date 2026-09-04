706aa83bc99d2b0e66758cc46326d6693fdfb983
deepak7989 <deepakvadla05@gmail.com>
2026-09-04 14:47:54 +0530

    api doc added

---
+++ 02-redis.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source ./common.sh
+
+dnf module disable redis -y &>> $LOGS_FILE 
+dnf module enable redis:7 -y &>> $LOGS_FILE
+dnf install redis -y &>> $LOGS_FILE
+VALIDATE $? "Installin Redis:7"
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e 's/protected-mode yes/protected-mode no/g' /etc/redis/redis.conf &>> $LOGS_FILE
+VALIDATE $? "Allowing remote coonections"
+
+systemctl enable redis &>> $LOGS_FILE
+systemctl start redis &>> $LOGS_FILE
+VALIDATE $? "Started Redis"
+
+print_total_time
