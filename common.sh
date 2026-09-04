070edf160b332594bb52b319dc21a8462e1b35cb
deepak7989 <deepakvadla05@gmail.com>
2026-09-04 14:17:03 +0530

    api doc added

---
+++ common.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+LOG_FOLDER="/var/log/roboshop" #creating folder #in this folder mongodb.sh will be created
+sudo mkdir -p $LOG_FOLDER #inside var/log
+sudo chown -R ec2-user:ec2-user $LOG_FOLDER # Set permission to ec2-user
+sudo chmod -R 755 $LOG_FOLDER
+LOGS_FILE="$LOG_FOLDER/$0.log"
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+
+echo -e "$TIMESTAMP [INFO] Script started"
+
+check_root(){
+    if [ $USERID -ne 0 ]; then
+        echo -e "$TIMESTAMP [ERROR] $R Please run this script with root access $N" | tee -a $LOGS_FILE
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$TIMESTAMP [ERROR] $2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$TIMESTAMP [INFO] $2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+print_total_time(){
+    echo -e "$TIMESTAMP [INFO] Script executed in $G $SECONDS seconds $N"
+}
